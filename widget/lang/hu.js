97df261dca83d1908c485ad9ec77c079f5aeba3c
atesz03 <139833210+atesz03@users.noreply.github.com>
2023-07-17 23:40:22 -0400

    Create hu.js

---
+++ widget/lang/hu.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'A hozzászólásokat működteti: Cusdis',
+  post_comment: 'Elküld',
+  loading: 'betöltés...',
+
+  // comment
+  email: 'E-mail cím (opcionális)',
+  nickname: 'Becenév',
+  reply_placeholder: 'Hozzászólás',
+  reply_btn: 'válasz',
+  sending: 'küldés...',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'A hozzászólás hiányzik!',
+  nickname_is_required: 'A becenév megadása kötelező',
+
+  comment_has_been_sent:
+    'A hozzászólásod el lett küldve. Kérjük várj a jóváhagyásra.',
+}
